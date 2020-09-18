19c56c639cf326c52f04508e3102759f8a68eca2
K1mskii <movies2702@gmail.com>
2020-09-18 18:41:21 +0600

    upd

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(".saying__sliders").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    loop: true,
+    prevArrow: $(".sliders__arrows-left"),
+    nextArrow: $(".sliders__arrows-right"),
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1134,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  });
+});
